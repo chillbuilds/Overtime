d46ae997d2876a1389f0ffb18055e9c74b5a4e74
chillbuilds <heliumface770@gmail.com>
2020-10-22 10:17:22 -0600

    added name method to user

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,21 +2,24 @@ require 'rails_helper'
 
 describe 'navigate' do
     before do
-        user = User.create(email:"test@test.com", password:"asdfasdf", password_confirmation: "asdfasdf", first_name:"Will", last_name:"Wojeck")
-        login_as(user, :scope => :user)
+        @user = User.create(email:"test@test.com", password:"asdfasdf", password_confirmation: "asdfasdf", first_name:"Will", last_name:"Wojeck")
+        login_as(@user, :scope => :user)
     end
+
     describe 'index' do
         it 'can be reached successfully' do
             visit posts_path
             expect(page.status_code).to eq(200)
         end
+
         it 'has a title of Posts' do
             visit posts_path
             expect(page).to have_content(/Posts/)
         end
+
         it 'has a list of posts' do
-            post1 = Post.create(date: Date.today, rationale: "something")
-            post2 = Post.create(date: Date.today, rationale: "something else")
+            post1 = Post.create(date: Date.today, rationale: "something", user_id: @user.id)
+            post2 = Post.create(date: Date.today, rationale: "something else", user_id: @user.id)
             visit posts_path
             expect(page).to have_content(/Post1|Post2/)
         end
